37ec5dd6ffeef36d917a2b704f22cc64eab1545d
Nassir Nowruzi <nassir.a.nowruzi@twoday.com>
2024-02-21 15:56:50 +0100

    Test footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <p>© {new Date().getFullYear()} Twoday</p>
         <div className="social-links">
           <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook  </a>
-          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter   </a>
           <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
         </div>
       </div>
