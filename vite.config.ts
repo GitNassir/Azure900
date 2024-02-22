c7e718a552ae8ae2910512ba8dda3737e3ec5197
Nassir Nowruzi <nassir.a.nowruzi@twoday.com>
2024-02-22 12:40:08 +0100

    Vite opdate

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path'; // Tilføj denne linje
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Tilføj dine aliaser her, hvis du har nogen
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // Du kan tilføje flere entry points her hvis nødvendigt
+      },
+    },
+    // base: '/min-undermappe/', // Kun nødvendig hvis du deployer til en undermappe
+  },
+});
