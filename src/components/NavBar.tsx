99f439456f030377a7bb88dcbc7e91b57b419a03
Nassir Nowruzi <nassir.a.nowruzi@twoday.com>
2024-02-22 11:30:59 +0100

    fsfs

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar: React.FC = () => {
     return (
         <nav className="navbar">
             <ul className="nav-links">
-                <li><Link to="/">Cloud Concept</Link></li>
+                <li><Link to="/home">Cloud Concept</Link></li>
                 <li><Link to="/compute-networking">Azure Core Services</Link></li>
                 <li><Link to="/azure-management">Azure Management Governance</Link></li>
                 <li><Link to="/quiz">Quiz</Link></li>
